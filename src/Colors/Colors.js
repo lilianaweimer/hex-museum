ed6d35623dab8c4e353646c23600333d679ee8b5
Liliana Weimer <lilianaweimer11@gmail.com>
2020-07-29 15:31:41 -0600

    Initial styling for Colors component

--- src/Colors/Colors.js
+++ src/Colors/Colors.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import './Colors.css'
 import { NavLink, Redirect } from 'react-router-dom';
 
 const Colors = (props) => {
-  
+  console.log(props)
   if (props.colors) {
     return (
-      <section className='colors'>
+      <>
         <NavLink to='/' className='home-nav'>home</NavLink>
-          {props.colors.map(color => {
+        <section className='colors'>
+          {props.colors.sort((a, b) => b.hex - a.hex).map(color => {
             return (
               <NavLink 
                 to={`/gallery/${color.hex}`} 
                 className='color-nav' 
                 key={color.id}
-                style={{border: `3px solid ${color.hex}`}}
+                style={{backgroundColor: color.hex}}
               >
-                <p>{color.name}</p>
-                <p>{color.hex}</p>
+                <p className='color-info'>{color.name}</p>
+                <p className='color-info'>{color.hex}</p>
               </NavLink>
             )
         })}
       </section>
+      </>
     )
   } else {
     return <Redirect to='/'/>
