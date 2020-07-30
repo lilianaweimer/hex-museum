0424ac8f9fdb37d66bed131af376bbe0f63f9e00
Liliana Weimer <lilianaweimer11@gmail.com>
2020-07-30 12:29:07 -0600

    Add mock fetch data to prevent too many calls to API

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,14 +1,32 @@
+import apikey from './apikey';
+import allColors from './MockAPIData/allColors.json';
+import colorGallery from './MockAPIData/colorGallery.json';
+import todaysColor from './MockAPIData/todaysColor.json';
+import replacement from './MockAPIData/replacementPiece.json';
+
 export const fetchTodaysColor = (day, apikey) => {
-  return fetch(`https://api.harvardartmuseums.org/spectrum/${day}?apikey=${apikey}`)
-    .then(response => response.json())
+  //temporary mock to avoid going over limit 
+  return Promise.resolve(todaysColor)
+  // return fetch(`https://api.harvardartmuseums.org/spectrum/${day}?apikey=${apikey}`)
+  //   .then(response => response.json())
 }
 
 export const getAllColors = (apikey) => {
-  return fetch(`https://api.harvardartmuseums.org/color?size=147&apikey=${apikey}`)
-  .then(response => response.json())
+  //temporary mock to avoid going over limit 
+  return Promise.resolve(allColors)
+  // return fetch(`https://api.harvardartmuseums.org/color?size=147&apikey=${apikey}`)
+  //   .then(response => response.json())
 }
 
 export const getArt = (color, apikey) => {
-  return fetch(`https://api.harvardartmuseums.org/object?color:${color}&hasImage=1&apikey=${apikey}`)
-    .then(response => response.json())
+  //temporary mock to avoid going over limit
+   return Promise.resolve(colorGallery)
+  // return fetch(`https://api.harvardartmuseums.org/object?q=imagepermissionlevel:0&color:${color}&hasImage=1&apikey=${apikey}`)
+  //   .then(response => response.json())
+}
+
+export const getReplacement = (color, apikey) => {
+  return Promise.resolve(replacement);
+  // return fetch(`https://api.harvardartmuseums.org/object?q=imagepermissionlevel:0&color:${color}&hasImage=1&apikey=${apikey}&size=1`)
+  //   .then(response => response.json())
 }
