9ee630bf629735f752ee40f04ed9ad19061f8476
Liliana Weimer <lilianaweimer11@gmail.com>
2020-08-03 14:19:07 -0600

    Make responsive mobile layout for Gallery

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,21 +5,21 @@ import todaysColor from './MockAPIData/todaysColor.json';
 
 export const fetchTodaysColor = (day) => {
   //temporary mock to avoid going over limit 
-  // return Promise.resolve(todaysColor)
-  return fetch(`https://api.harvardartmuseums.org/spectrum/${day}?apikey=${apikey}`)
-    .then(response => response.json())
+  return Promise.resolve(todaysColor)
+  // return fetch(`https://api.harvardartmuseums.org/spectrum/${day}?apikey=${apikey}`)
+  //   .then(response => response.json())
 }
 
 export const getAllColors = () => {
   //temporary mock to avoid going over limit 
-  // return Promise.resolve(allColors)
-  return fetch(`https://api.harvardartmuseums.org/color?size=147&apikey=${apikey}`)
-    .then(response => response.json())
+  return Promise.resolve(allColors)
+  // return fetch(`https://api.harvardartmuseums.org/color?size=147&apikey=${apikey}`)
+  //   .then(response => response.json())
 }
 
 export const getArt = (color) => {
   //temporary mock to avoid going over limit
-  //  return Promise.resolve(colorGallery)
-  return fetch(`https://api.harvardartmuseums.org/object?color:${color}&apikey=${apikey}&hasImage=1&sort=random&q=imagepermissionlevel:0`)
-    .then(response => response.json())
+   return Promise.resolve(colorGallery)
+  // return fetch(`https://api.harvardartmuseums.org/object?color:${color}&apikey=${apikey}&hasImage=1&sort=random&q=imagepermissionlevel:0`)
+  //   .then(response => response.json())
 }
