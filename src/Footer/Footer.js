ddab9c8dc9cd0eeef53a6ed71a98f9e2a66912e4
Liliana Weimer <lilianaweimer11@gmail.com>
2020-08-03 12:58:22 -0600

    Lint files in Footer directory

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,12 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <p className='footer'>all art is from the <a href='https://www.harvardartmuseums.org/'>harvard art museum</a></p>
+    <p className='footer'>
+      all art is from the 
+      <a href='https://www.harvardartmuseums.org/'>
+        harvard art museum
+      </a>
+    </p>
   )
 }
 
